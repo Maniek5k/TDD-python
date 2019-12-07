dbe5331356541bffb0b18f3f685ad346df90f326
maniek5k <maniek5k@users.noreply.github.com>
2019-12-07 13:24:10 +0100

    Add tests

---
+++ strings/strings.py
@@ -0,0 +1,20 @@
+# def return_upper(param):
+#     x = param.upper()
+#     return x
+#
+#
+# def return_lower(param):
+#     x = param.lower()
+#     return x
+#
+# def return_capitalize(param):
+#     x = param.capitalize()
+#     return x
+#
+# def return_length(param):
+#     x = len(param)
+#     return x
+#
+# def return_title(param):
+#     x = param.title()
+#     return x
