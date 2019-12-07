e42338699f37e5fecf20da884b29412388762c13
maniek5k <maniek5k@users.noreply.github.com>
2019-12-07 11:54:17 +0100

    Add calculator module

--- test_program.py
+++ test_program.py
@@ -21,12 +21,9 @@ class TestProgram(unittest.TestCase):
 
     def test_calc_sum(self):
         rand1 = random.randint(1, 5)
-        print(rand1)
         rand2 = random.randint(5, 50)
-        print(rand2)
         calc = program.calc_sum(rand1, rand2)
-        print(calc)
-        # self.assertEqual(calc, sum(rand1, rand2))
+        self.assertEqual(calc, (rand1 + rand2))
 
     def test_calc_diff(self):
         diff = program.calc_diff(5, 2)
