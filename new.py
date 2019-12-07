77683c583af0d6bb3fc56e07d0d84316b05555d0
maniek5k <maniek5k@users.noreply.github.com>
2019-12-07 09:50:47 +0100

    Update new

--- new.py
+++ new.py
@@ -1,8 +1,12 @@
-input = input()
+import random
 
-if int(input) > 5:
+player = int(input())
+
+if player > 5:
     print("More than 5")
-elif int(input) == 5:
+elif player == 5:
     print("Exactly 5")
 else:
     print("Less than 5")
+
+print(random.choice(["Andrzej", "Adam", "Zygmunt"]))
