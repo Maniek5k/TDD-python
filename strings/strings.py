a37819cb6173829d3e4fb122860421eeb3d99fbf
maniek5k <maniek5k@users.noreply.github.com>
2019-12-07 14:43:39 +0100

    Fix

--- strings/strings.py
+++ strings/strings.py
@@ -1,19 +1,20 @@
 
-
-import os
-
 def return_upper(s):
     return s.upper()
+
 
 def return_lower(s):
     return s.lower()
 
+
 def return_capitalize(s):
     return s.capitalize()
+
 
 def return_length(s):
     s=len(s)
     return s
+
 
 def return_title(s):
     return s.title()
